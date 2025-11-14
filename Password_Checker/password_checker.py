8dc0c5edf77cf9766463bd300f4482d2e9e43156
Diya <diyashah2914@gmail.com>
2025-11-14 20:27:29 +0000

    add checks for uppercase,lowercase,digits,and symbols

---
+++ Password_Checker/password_checker.py
@@ -0,0 +1,40 @@
+import string
+# password= input("Enter your password: ")
+
+while True:
+    password= input("Enter your password : ")
+
+    
+    if len(password) < 8:
+        print("Password must be at least 8 characters long.")
+        print("Enter a valid password.")
+
+    elif len(password) >=8 and len(password) <=10:
+            print("Password strength: Weak")
+            print("Your password: ",password)
+            break
+    elif len(password) >10 and len(password) <=14:  
+            print("Password strength: Moderate")
+            print("Your password: ",password)
+            break
+    elif len(password) >14 and len(password) <=16:
+            print("Password strength: Strong")
+            print("Your password: ",password)
+            break
+    else:
+            print("Password must not exceed 16 characters.")
+            print("Enter a valid password.")
+
+
+print("Lowercase: " ,password.islower())
+print("Uppercase: ", password.isupper())
+print("Digit:     " ,password.isalnum())
+has_symbol = any(char in string.punctuation for char in password)
+print("Symbol:    ",has_symbol)
+
+
+
+    
+
+
+    
